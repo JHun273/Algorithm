f9c7f3a631700ea17a544ef02c011b70ca9a18b5
JHun273 <rlawogns273@gmail.com>
2024-02-16 15:31:27 +0900

    큐

---
+++ 큐/BOJ_1021.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+deque<int> DQ;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin>>n;
+    cin>>m;
+
+    for(int i = 1; i<=n; i++)
+    {
+        DQ.push_back(i);
+    }
+
+    //그냥 구현하고 숫자 넣어라. 끝
+}
